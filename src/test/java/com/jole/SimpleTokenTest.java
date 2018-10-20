a8972ef18a32e7670a70e557d3a37875ff197409
joklek <31686813+joklek@users.noreply.github.com>
2018-10-20 15:44:40 +0300

    add simple token tests

---
+++ src/test/java/com/jole/SimpleTokenTest.java
@@ -0,0 +1,58 @@
+package com.jole;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static com.jole.TokenType.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class SimpleTokenTest {
+
+    private static Stream<Arguments> tokensAndExpectedTypes() {
+        return Stream.of(
+                arguments("(", LEFT_PAREN),
+                arguments(")", RIGHT_PAREN),
+                arguments("{", CURLY_LEFT),
+                arguments("}", CURLY_RIGHT),
+                arguments("[", LEFT_BRACE),
+                arguments("]", RIGHT_BRACE),
+                arguments(",", COMMA),
+                arguments(";", SEMICOLON),
+
+                arguments("+", PLUS),
+                arguments("+=", PLUS_EQUAL),
+                arguments("-", MINUS),
+                arguments("-=", MINUS_EQUAL),
+                arguments("*", STAR),
+                arguments("*=", MUL_EQUAL),
+                arguments("%", MOD),
+                arguments("%=", MOD_EQUAL),
+                arguments("=", EQUAL),
+                arguments("==", EQUAL_EQUAL),
+
+                arguments(">", GREATER),
+                arguments(">=", GREATER_EQUAL),
+                arguments(">>", INPUT_SIGN),
+
+                arguments("<", LESS),
+                arguments("<=", LESS_EQUAL),
+                arguments("<<", OUTPUT_SIGN),
+
+                arguments("/=", DIV_EQUAL),
+                arguments("/", SLASH)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("tokensAndExpectedTypes")
+    void shouldGetCorrectTokenType(String token, TokenType expectedType) {
+        Scanner scanner = new Scanner(token);
+        ScannerResults scannerResults = scanner.scanTokens();
+        assertThat(scannerResults.getTokens().get(0).getType(), is(expectedType));
+    }
+}
